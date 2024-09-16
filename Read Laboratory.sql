f1589c2e281e6822a8af9ea9fc3bee50355314ee
nissycode <158865246+nissycode@users.noreply.github.com>
2024-09-16 19:48:49 +0800

    Update Read Laboratory.sql

--- Read Laboratory.sql
+++ Read Laboratory.sql
@@ -1,3 +1,16 @@
+create database dbcompany;
+
+create table tblemployees(
+  UID int primary key auto_increment,
+  FirstName varchar(45) not null,
+  LastName varchar(45) not null,
+  age int not null,
+  phoneNumber varchar(11),
+  address varchar(45) not null,
+  birthday date not null,
+  salary varchar(45) not null,
+  email varchar(45) not null
+  );
 
 /* 5 value, no duplicate */
 insert into tblemployees(
@@ -9,4 +22,4 @@ address,
 birthday,
 salary,
 email,
-hiredOn) VALUES ("Maloi", "Ricale", 23, "09567894056", "Quezon City", "2002-02-14", "30,000", "binimaloi@gmail.com", "2023-12-20")
+hiredOn) VALUES ("Maloi", "Ricale", 23, "09567894056", "Quezon City", "2002-02-14", "30,000", "binimaloi@gmail.com", "2023-12-20");
