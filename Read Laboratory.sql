138294c9c8b433f74566e029430ab299f52f0fa2
nissycode <158865246+nissycode@users.noreply.github.com>
2024-09-16 19:46:09 +0800

    Add files via upload

---
+++ Read Laboratory.sql
@@ -0,0 +1,12 @@
+
+/* 5 value, no duplicate */
+insert into tblemployees(
+FirstName,
+LastName,
+age,
+phoneNumber,
+address,
+birthday,
+salary,
+email,
+hiredOn) VALUES ("Maloi", "Ricale", 23, "09567894056", "Quezon City", "2002-02-14", "30,000", "binimaloi@gmail.com", "2023-12-20")
